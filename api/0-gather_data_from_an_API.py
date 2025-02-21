f8498a689bcb056b0b2b6da94c3ad7832668aaeb
Emery <e.barame@alustudent.com>
2025-02-21 20:59:12 +0200

    export data from csv to json

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,25 @@
+
+#!/usr/bin/python3
+
+"""gather data from an api"""
+import requests
+import sys
+
+if __name__ == '__main__':
+    """gather data from an api"""
+    response_todos = requests.get(
+        "https://jsonplaceholder.typicode.com/user/" + sys.argv[1] + "/todos"
+    )
+    data = response_todos.json()
+    completed_tasks = []
+    for i in data:
+        if i['completed']:
+            completed_tasks.append(i['title'])
+
+    user = requests.get(
+        "https://jsonplaceholder.typicode.com/users/" + sys.argv[1]
+    ).json()
+    print("Employee {} is done with tasks({}/{}):".
+          format(user["name"], len(completed_tasks), len(data)))
+    [print("\t " + task) for task in completed_tasks]
+
